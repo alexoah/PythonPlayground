1bb7c49a2bc122643973af86fb651296ac53b772
Alexoah YT <alexandra.soah@gmail.com>
2020-03-21 16:14:51 +0700

    added answer file for PYTHON Lambda exercise

---
+++ W3School-PyExercises/PY-Lambda/pyLambdaE1.py
@@ -0,0 +1,9 @@
+"""
+from PYTHON Lambda: Exercise 1 ( https://www.w3schools.com/python/exercise.asp?filename=exercise_lambda1 )
+
+question:
+    Create a lambda function that takes one parameter (a) and returns it.
+
+    x = ______ _ _ _
+"""
+x = lambda a : a
